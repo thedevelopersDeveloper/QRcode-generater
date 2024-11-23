d55dc7e67c0a269ce5b4a7e58404ef50a8c9837f
Ashirwad Dwivedi <ashirwaddwi5510@gmail.com>
2024-11-23 16:07:31 +0000

    second commit

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ let url = ``
 let img = document.querySelector(".img")
 let input = document.querySelector(".qr-input")
 let btn = document.querySelector(".generate-btn")
-
+let download = document.querySelector(".download")
 btn.addEventListener("click",()=>{
     data = input.value;
     if(data!=""){
         url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}`
         img.src=url
+        download.style.display = "block";
     }
 
 })
