a16cd15ab81865e022eda1930bf185c307ab83a2
Ashirwad Dwivedi <ashirwaddwi5510@gmail.com>
2024-11-23 15:42:55 +0000

    On branch main   Changes to be committed: 	deleted:    README.md 	new file:   index.html 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,14 @@
+let data = ""
+let url = ``
+let img = document.querySelector(".img")
+let input = document.querySelector(".qr-input")
+let btn = document.querySelector(".generate-btn")
+
+btn.addEventListener("click",()=>{
+    data = input.value;
+    if(data!=""){
+        url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}`
+        img.src=url
+    }
+
+})
